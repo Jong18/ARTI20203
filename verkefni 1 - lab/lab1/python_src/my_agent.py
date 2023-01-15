ac125d991d95bb35fdeac57604792c956295f1c4
Ingólfur Ari Jóhannsson <ingoar97@gmail.com>
2023-01-15 22:49:17 +0000

    lausnir eins og er

--- verkefni 1 - lab/lab1/python_src/my_agent.py
+++ verkefni 1 - lab/lab1/python_src/my_agent.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from agent import Agent
 from enum import IntEnum
 from copy import deepcopy
@@ -21,7 +23,8 @@ class Point:
     def __eq__(self, other):
         """ To check position == home """
         return self.x == other.x and self.y == other.y
-
+    def __sub__(self, other):
+        return self.y - other.y
     def move(self, orientation, step=1):
         """ Updates position (use if elif for python < 3.10) """
         match orientation:
@@ -35,19 +38,27 @@ class Point:
                 self.x -= step
 
 class MyAgent(Agent):
+
     def __init__(self):
         self.position = Point()
         self.home = Point()
         self.orientation = Orientation.NORTH
         self.turned_on = False
+        self.ET_go_home = False
 
         self.visited = {self.home}
+        self.ignore = {self.home}
+
+        self.turn_bool = False
+        self.turn_count = 0
+        self.turn_direction = "right"
 
         self.bump_counter = 0
         self.max_y = None
-        self.max_x = None
-        self.min_x = None
         self.min_y = None
+        self.size = 1000000
+        self.turn_occurances = 0
+
 
 
 
@@ -56,7 +67,23 @@ class MyAgent(Agent):
         pass
 
     def cleanup(self, percepts):
+        self.position = Point()
+        self.home = Point()
         self.orientation = Orientation.NORTH
+        self.turned_on = False
+
+        self.visited = {self.home}
+        self.ignore = {self.home}
+
+        self.turn_bool = False
+        self.turn_count = 0
+        self.turn_direction = "right"
+
+        self.bump_counter = 0
+        self.max_y = None
+        self.min_y = None
+        self.size = 1000000
+        self.turn_occurances = 0
 
     def turn_on(self):
         self.turned_on = True
@@ -82,33 +109,68 @@ class MyAgent(Agent):
     def undo_move(self):
         self.visited.remove(self.position)
         self.position.move(self.orientation, -1)
-
     def next_action(self, percepts):
-        print(f"Percepts: {percepts}")
 
+        print(f"Percepts: {percepts}")
         if not self.turned_on:
             return self.turn_on()
-
         if "DIRT" in percepts:
             return "SUCK"
 
-        if "BUMP" in percepts :
+        if self.ET_go_home:
+            if self.position.x == self.home.x and self.position.y == self.home.y:
+                self.cleanup()
+            elif self.position.x <= self.home.x:
+                self.orientation = Orientation.EAST
+            elif self.position.x >= self.home.x:
+                self.orientation = Orientation.WEST
+            elif self.position.x == self.home.x and self.position.y <= self.home.y:
+                self.orientation = Orientation.NORTH
+            elif self.position.x == self.home.x and self.position.y >= self.home.y:
+                self.orientation = Orientation.SOUTH
+            return self.go()
+        if self.turn_occurances == self.size:
+            self.turn_bool = False
+            self.ET_go_home = True
+
+        if self.turn_bool and self.turn_count < 3:
+            self.turn_count += 1
+            if self.turn_direction == "right" and self.turn_count == 1:
+                return self.turn_right()
+            elif self.turn_direction == "right" and self.turn_count == 2:
+                return self.go()
+            elif self.turn_direction == "right" and self.turn_count == 3:
+                self.turn_direction = "left"
+                self.turn_occurances += 1
+                return self.turn_right()
+            if self.turn_direction == "left" and self.turn_count == 1:
+                return self.turn_left()
+            elif self.turn_direction == "left" and self.turn_count == 2:
+                return self.go()
+            elif self.turn_direction == "left" and self.turn_count == 3:
+                self.turn_direction = "right"
+                self.turn_occurances += 1
+                return self.turn_left()
+        else :
+            self.turn_count = 0
+            self.turn_bool = False
+
+        if self.bump_counter == 0:
+            self.ignore.add(self.position)
+
+        if ("BUMP" in percepts ) :
+            # or self.position in self.visited
+            #and not (self.position in self.ignore)
             self.undo_move()
             self.bump_counter += 1
             if self.bump_counter == 1:
-                self.max_y = self.position
-                self.turn_right()
+                self.max_y = deepcopy(self.position)
+                return self.turn_right()
             elif self.bump_counter == 2:
-                self.max_x = self.position
-                self.turn_right()
+                return self.turn_right()
             elif self.bump_counter == 3:
-                self.min_y = self.position
-                self.turn_right()
-            elif self.bump_counter == 4:
-                self.min_x = self.position
-                self.turn_right()
-                self.go()
-                self.turn_right()
+                self.min_y = deepcopy(self.position)
+                self.size = self.max_y - self.min_y
             else:
-                pass
+                self.turn_bool = True
         return self.go()
